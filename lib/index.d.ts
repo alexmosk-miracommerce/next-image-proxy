40f4cdcf2967dd5f4f6b576ac3c12419710aa057
Alex Mosk <alex.mosk@miracommerce.com>
2022-11-09 15:44:28 -0500

    remove build from main branch

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { DeepPartial, Options } from './types';
-export declare function withImageProxy(passedOptions?: DeepPartial<Options>): (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
